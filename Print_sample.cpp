3ac901905e67228fb29763964f7b73229c0c20ec
Biven21 <igbochkarev2017@gmail.com>
2021-10-05 17:20:43 +0400

    PrintArray - написал функцию

--- Print_sample.cpp
+++ Print_sample.cpp
@@ -2,22 +2,14 @@
 #include "TXLib.h"
 
 void FillArray (int data[], int size, int start, int step);
+void PrintArray (const int data[], int size);
 
 int main ()
     {
     int data [20] = {};
 
     FillArray (data, 20, 0, 10);
-
-    for (int i = 0; i < 20; i++)
-        {
-        if (i % 5 == 0)
-            {
-            printf ("\n");
-            }
-        $g; printf ("[%2d] = ", i);
-        $c; printf ("%3d,", data [i]);
-        }
+    PrintArray (data, 20);
 
     return 0;
     }
@@ -26,4 +18,17 @@ void FillArray (int data[], int size, int start, int step)
      {
      for (int i = 0; i < size; i++)
          data [i] = start + i* step;
+     }
+
+void PrintArray (const int data[], int size)
+     {
+     for (int i = 0; i < size; i++)
+         {
+         if (i % 5 == 0)
+             {
+             printf ("\n");
+             }
+         $g; printf ("[%2d] = ", i);
+         $c; printf ("%3d,", data [i]);
+         }
      }
