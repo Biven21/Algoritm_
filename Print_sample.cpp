01d22d2218d9f5eedc1fb16a30e7b5f8b6e9110a
Biven21 <igbochkarev2017@gmail.com>
2021-10-06 20:25:23 +0400

    добавил надписи

--- Print_sample.cpp
+++ Print_sample.cpp
@@ -2,29 +2,38 @@
 #include "TXLib.h"
 
 void FillArray (int data[], int size, int start, int step);
-void PrintArray (const int data[], int size);
+void PrintArray (const int data[], int size, const char title[]);
 
 int main ()
     {
     int data [20] = {};
 
     FillArray  (data, 20, 0, 10);
-    PrintArray (data, 20);
+    PrintArray (data, 20, "после заполнения");
     printf ("\n");
     FillArray  (data, 30, 0, 10);
-    PrintArray (data, 30);
+    PrintArray (data, 30, "после заполнения");
 
     return 0;
     }
 
 void FillArray (int data[], int size, int start, int step)
      {
+     data [0] = start;
+
      for (int i = 0; i < size; i++)
-         data [i] = start + i* step;
+         {
+         assert (0 <= i   && i   < size);
+         assert (0 <= i-1 && i-1 < size);
+
+         data [i] = data [i - 1] + step;
+         }
      }
 
-void PrintArray (const int data[], int size)
+void PrintArray (const int data[], int size, const char title[])
      {
+     printf ("\n%s:", title);
+
      for (int i = 0; i < size; i++)
          {
          if (i % 5 == 0)
