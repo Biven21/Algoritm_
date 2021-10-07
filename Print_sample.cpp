d0827641c21ccc5bbab88c6397d477bc5c5e7f94
Biven21 <igbochkarev2017@gmail.com>
2021-10-07 15:42:06 +0400

    FillArray123321 с отладкой

--- Print_sample.cpp
+++ Print_sample.cpp
@@ -4,6 +4,7 @@
 
 void FillArray (int data[], int size, int start, int step);
 void FillArray123 (int data123[], int size);
+void FillArray123321 (int data123321[], int size);
 
 int main ()
     {
@@ -22,8 +23,14 @@ int main ()
     FillArray123 (data123, 10);
     PrintArray (data123, 10, 3, "после заполнения");
 
+    int data123321 [55] = {};
+    FillArray123321 (data123321, 55);
+    PrintArray (data123321, 55, 6, "после заполнения");
+
     return 0;
     }
+
+//-----------------------------------------------------------------------------
 
 void FillArray (int data[], int size, int start, int step)
      {
@@ -37,6 +44,9 @@ void FillArray (int data[], int size, int start, int step)
          data [i] = data [i - 1] + step;
          }
      }
+
+//-----------------------------------------------------------------------------
+
 void FillArray123 (int data123[], int size)
     {
     data123 [0] = 0;
@@ -45,6 +55,30 @@ void FillArray123 (int data123[], int size)
         {
         assert (0 <= i && i < size);
 
-        data123 [i] = i%3 + 1;
+        data123 [i] = i %3 + 1;
         }
     }
+
+//-----------------------------------------------------------------------------
+
+void FillArray123321 (int data123321[], int size)
+     {
+     data123321 [0] = 0;
+     int mamber = 0;
+     int period = 0;
+
+     for (int i = 0; i < size; i++)
+         {
+         mamber = i %3 + 1;
+
+         printf ("\n %d член последовательности %d  ", i, mamber);
+
+         if (mamber == 1)
+             period = period + 1;
+         printf ("\n %d period %d  ", i, period);
+
+         if (period %2 == 1) data123321 [i] =      mamber %4;
+         if (period %2 == 0) data123321 [i] = -1 * mamber %4 + 4;
+         }
+     }
+
