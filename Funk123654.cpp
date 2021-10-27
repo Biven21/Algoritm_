8d331606b58069068c4842d303cb4765afdfe9e0
Biven21 <igbochkarev2017@gmail.com>
2021-10-27 16:07:31 +0400

    выделил функцию

--- Funk123654.cpp
+++ Funk123654.cpp
@@ -2,18 +2,28 @@
 #include "TXLib.h"
 #include "Algofunc.h"
 
+void FillFerstAr (int data [], int size);
+
  int main ()
      {
      int size = 23;
-     int data [size] = {};
+     int data [23] = {};
 
+     FillFerstAr (data, 23);
+
+     PrintArray (data, 20, 3, "после заполнения");
+     }
+
+//-----------------------------------------------------------------------------
+
+void FillFerstAr (int data [], int size)
+     {
      data [0] = 1;
      int i = 1;
 
-     for (1 <= i; i >= size; i++)
+     for (1 <= i; i <= size; i++)
          {
-         if (i % 4 != 0) data [i] = data [i - 1] + 1;
+         if (i % 3 != 0) data [i] = data [i - 1] + 1;
          else            data [i] = data [i - 1] + 4;
          }
-     PrintArray (data, 20, 5, "после заполнения");
      }
