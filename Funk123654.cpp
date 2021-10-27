785dfbd9486c4fe12759e0e5f9a7dbe033c1f218
Biven21 <igbochkarev2017@gmail.com>
2021-10-27 17:43:40 +0400

    футбол

--- Funk123654.cpp
+++ Funk123654.cpp
@@ -11,7 +11,7 @@ void FillFerstAr (int data [], int size);
 
      FillFerstAr (data, 23);
 
-     PrintArray (data, 20, 3, "после заполнения");
+     PrintArray (data, 23, 3, "после заполнения");
      }
 
 //-----------------------------------------------------------------------------
@@ -21,9 +21,15 @@ void FillFerstAr (int data [], int size)
      data [0] = 1;
      int i = 1;
 
-     for (1 <= i; i <= size; i++)
+     for (1 <= i; i <= (size + 1) / 2; i++)
          {
          if (i % 3 != 0) data [i] = data [i - 1] + 1;
          else            data [i] = data [i - 1] + 4;
+         }
+
+     for ((size + 1) / 2 < i; i <= size; i++)
+         {
+         if (i % 3 != 0) data [i] = data [i - 1] - 1;
+         else            data [i] = data [i - 1] - 4;
          }
      }
