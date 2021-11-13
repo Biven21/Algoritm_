687afc092e443a8b3ebd852cda47f42a358380dd
Biven21 <igbochkarev2017@gmail.com>
2021-11-13 15:37:26 +0400

    in villa

--- Funk123654.cpp
+++ Funk123654.cpp
@@ -3,7 +3,7 @@
 #include "Algofunc.h"
 
 void FillArr (int data [], int size, int SizeNch, int Sizech);
-void DataDoun (int Sizech, int dd []);
+int DataDoun (int dd [], int Sizech);
 
  int main ()
      {
@@ -27,7 +27,8 @@ void DataDoun (int Sizech, int dd []);
          }
      else Sizech = ChPer * 3;
 
-     printf ("\n колво нечетн членов SizeNch = %d \n колво четных членов = %d \n сдвиг = %d", SizeNch, Sizech, shift);
+     printf ("\n колво нечетн членов SizeNch = %d \n колво четных членов = %d \n сдвиг = %d",
+             SizeNch, Sizech, shift);
 
      FillArr (data, 16, SizeNch, Sizech);
 
@@ -52,17 +53,23 @@ void FillArr (int data [], int size, int SizeNch, int Sizech)
          printf ("\n data [i] = %d  i = %d", data [i], i);
          }
 
-     DataDoun (Sizech, dd);
+     DataDoun (dd, Sizech);
 
      PrintArray (dd, Sizech, 6, "после заполнения");
 
-     int j = 0;
+     int j = Sizech - 1;
+
+     printf ("\n\n dd [0] = %d \n dd [j=0] = %d \n", dd [0], dd [j]);
+     printf ("\n dd [1] = %2d j = %d \n", dd [j+1], j + 1);
+     printf ("\n dd [2] = %2d j = %d \n", dd [j+2], j + 2);
+     printf ("\n dd [3] = %2d j = %d \n", dd [j+3], j + 3);
+
      int i = SizeNch;
      for (i = SizeNch;  i < size; i++)
          {
-         printf ("\n dd[j] = %d", dd [j]);
+         $y printf ("\n dd[j] = %d     j = %d", dd [j], j);
 
-         assert (SizeNch <= i  && i < size);
+         assert (SizeNch <= i && i < size);
 
 
          data [i] = dd [j];
@@ -70,24 +77,26 @@ void FillArr (int data [], int size, int SizeNch, int Sizech)
          printf ("\n data[i] = %d", data [i]);
          printf ("\n");
 
-         j++;
+         j--;
          }
      }
 
 //-----------------------------------------------------------------------------
 
-void DataDoun (int Sizech, int dd [])
+int DataDoun (int dd [], int Sizech)
      {
      dd [0] = 4;
 
      for (int j = 1; j < Sizech; j++)
          {
-         assert (0 <= j-1 && j-1 < Sizech);
+         assert (0 <= j     && j     < Sizech);
+         assert (0 <= j - 1 && j - 1 < Sizech);
 
          if (j % 3 != 0) dd [j] = dd [j - 1] + 1;
          else            dd [j] = dd [j - 1] + 4;
 
          printf ("\n\n dd [j] = %d  j = %d", dd [j], j);
          }
+     return 0;
      }
 //D:\school\КПК\Репозиторий\Algoritm_\Funk123654.cpp|58|error: expected primary-expression before 'int'|
